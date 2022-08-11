1705c7a822981e2d247e3c1c946371e99b1c8553
laurenp305 <lamor1800@gmail.com>
2022-08-11 19:30:32 -0400

    added questions for readme in js

---
+++ index.js
@@ -0,0 +1,25 @@
+const inquirer = require("inquirer");
+
+const questions = [
+{ 
+    type: "input",
+    name: "title",
+    message: "What's are you naming your project?",
+},
+{
+    type: "input",
+    name: "title",
+    message: "What's the description of your project?",
+},
+{
+    type: "input",
+    name: "title",
+    message: "What is the license of your project?",
+},
+{
+    type: "input",
+    name: "title",
+    message: "What is your github repository URL?",
+},
+];
+
